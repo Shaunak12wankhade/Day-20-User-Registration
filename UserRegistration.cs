40095c0975d30d34e24d073de44469b662e5f5a9
unknown <shaunak12wankhade@ggmail.com>
2021-10-29 11:22:41 +0530

    [Shaunak] Added : Password rule 4 of 1 special character

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -12,7 +12,7 @@ namespace Day19UserRegistration
         public static string Regex_LastName = "^[A-Z][a-z]{2,}$";
         public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public static string Regex_Number = "^[0-9]{2}[ ][6-9][0-9]{9}$";
-        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.{8,}))";
+        public static string Regex_Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))";
 
 
         public static bool ValidateFirstName(string FirstName)
@@ -65,7 +65,7 @@ namespace Day19UserRegistration
         {
             if (Regex.IsMatch(number, Regex_Number) == false)
             {
-                Console.WriteLine("Enter valid Mobile number!");
+                Console.WriteLine("Enter valid number!");
             }
         }
 
@@ -79,7 +79,7 @@ namespace Day19UserRegistration
             if (Regex.IsMatch(password, Regex_Password) == false)
             {
                 Console.WriteLine("Enter valid password. It should contain minimum 8 character," +
-                    "atleast one upper case character and atleast one numeric value");
+                    "atleast one upper case character and atleast one numeric value and atleast one special character");
             }
         }
 
